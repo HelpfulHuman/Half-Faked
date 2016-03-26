2d89cf0fbf525848beb456ace82fa2a20799db80
Nick Glenn <nick@helpfulhuman.com>
2016-03-26 04:21:19 -0700

    Adjusting some build settings, getting ready to publish

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,12 @@ import babel from 'rollup-plugin-babel';
 export default {
   entry: 'src/index.js',
   format: 'cjs',
-  plugins: [ babel() ],
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: ['es2015-rollup'],
+      plugins: ['transform-object-rest-spread']
+    })
+  ],
   dest: 'dist/index.js'
 };
